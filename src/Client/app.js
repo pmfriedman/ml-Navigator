c18d04a1b9ac295268592644a6fbf1c62081f0ea
pmfriedman <philipmfriedman@gmail.com>
2014-04-29 14:27:13 -0400

    navbar

--- src/Client/app.js
+++ src/Client/app.js
@@ -19,3 +19,15 @@ app.config(['$routeProvider', 'urlPrefix', function($routeProvider, urlPrefix) {
 		});
 
 }]);
+
+app.controller('navController', ['$scope', '$location', 'urlPrefix', function($scope, $location, urlPrefix){
+
+	$scope.areas = [{name: 'Documents', url: urlPrefix.navigator}];
+
+	// set the 'active' class if the button's link matches the prefix of the current url
+	$scope.navClass = function(urlPrefix) {
+		var currentRoute = $location.path();
+		return currentRoute.substring(0, urlPrefix.length) === urlPrefix ? 'active' : '';
+	}
+	
+}]);
